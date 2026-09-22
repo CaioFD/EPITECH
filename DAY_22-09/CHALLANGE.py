271292ff0e900753dc07bbff8d374c048bdb0aae
Caio Diniz <caio.diniz@epitech.eu>
2026-09-22 12:17:39 +0200

    Fixing errors
    Signed-off-by: Caio Diniz <caio.diniz@epitech.eu>

--- DAY_22-09/CHALLANGE.py
+++ DAY_22-09/CHALLANGE.py
@@ -1,9 +1,36 @@
-import math
+def gcd(a, b): #gratest common divisor
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b): #lowest common multiple
+    return a * b // gcd(a, b)
+
+def lcm_range(n): 
+    result = 1
+    for i in range(1, n + 1):
+        result = lcm(result, i)
+    return result
+
 print("LCM of numbers:") #lowest common multiple
-print("Range of 1 to 21:")
-print(math.lcm(*range(1, 21)))    
+print("Range of 1 to 20:")
+print(lcm_range(20))
 print("Range of 1 to 200:")
-print(math.lcm(*range(1, 201)))   
+print(lcm_range(200))
 print("Range of 1 to 2000:")
-print(math.lcm(*range(1, 2001)))  
-#do again
+print(lcm_range(2000))
+
+
+def gcd (a,b): 
+    while b:
+      a,b = b, a % b
+    return a
+
+def lcm(a,b):
+    return a * b // gcd(a,b)
+
+def lcm_range(b):
+    result = 1 
+    for i in range(1,b+1):
+        result = lcm(result,i)
+    return result
